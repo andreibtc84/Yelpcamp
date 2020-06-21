32c0f38795b20cb5e96f3800754b954d17fe32b8
root <andreibtc@gmail.com>
2020-06-21 14:48:37 +0000

    Added DB and modified app.js to connect with DB

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose")
 
+
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+// Schema Setup
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Granite Hill:", 
+// 		image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("Newly created campground: ");
+// 			console.log(campground);
+// 		}
+// 	});
 
 var campgrounds = [
 		{name: "Salmon Creek:", image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"},
@@ -23,9 +50,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-	
-	
-	res.render("campgrounds", {campgrounds:campgrounds});
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds:allCampgrounds});
+		}
+	})
+	// res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
@@ -35,9 +68,16 @@ app.post("/campgrounds", function(req, res){
 	var image = req.body.image;
 	var newCampground = {name: name, image: image}
 	
-	campgrounds.push(newCampground);
-	// redirect back to campgrounds page 
-	res.redirect("/campgrounds");
+	//Create a new Campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else{
+			// redirect back to campgrounds page 
+			res.redirect("/campgrounds");
+		}
+	});
+	
 });
 
 app.get("/campgrounds/new", function(req, res){
