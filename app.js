67acda65045a196169be563d6fc15a9482167c5b
root <andreibtc@gmail.com>
2020-08-23 13:10:50 +0000

    Configured passport/ added reg routes and template

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "Secret passcode",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 // Campground.create(
@@ -147,7 +158,30 @@ app.post("/campgrounds/:id/comments", function(req, res){
    });
    
 });
-	
+
+//============
+// AUTH ROUTES
+// ===========
+
+// show register form
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+// handle sign up logic
+ app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register")
+		} 
+		 passport.authenticate("local")(req, res, function(){
+			 res.redirect("/campgrounds");
+		 });
+	});
+ });
+
 app.listen(3000, function() { 
   console.log('YelpCamp Server listening on port 3000'); 
 });
