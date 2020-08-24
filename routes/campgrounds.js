1e56a45569981d222542df0084a9fe4decc8598c
root <andreibtc@gmail.com>
2020-08-24 17:59:03 +0000

    Prevent non logged in users from creating campground / Save username+id to newly created campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,13 +19,17 @@ router.get("/", function(req, res){
 });
 
 // Create route - add new campgrounds to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	
 	// get data from form and add to campgrounds array
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc}
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = {name: name, image: image, description: desc, author:author}
 	
 //Create a new Campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
@@ -42,7 +46,7 @@ router.post("/", function(req, res){
 
 //NEW - show form to create new campground
 
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
 	res.render("campgrounds/new")
 });
 
@@ -62,5 +66,14 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+// Middleware - check if user is logged in
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}  
 
 module.exports = router;
