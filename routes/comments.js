fddc8279328b62c46b52c0234c7083c6b32bd07b
root <andreibtc@gmail.com>
2020-08-26 10:41:26 +0000

    Added Delete comment functionality

--- routes/comments.js
+++ routes/comments.js
@@ -73,6 +73,18 @@ router.put("/:comment_id" , function(req, res){
 	});
 });
 
+// Comment Destroy Route
+router.delete("/:comment_id", function(req, res){
+	//findByIdAndRemove
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 // Middleware - check if user is logged in
 
 function isLoggedIn(req, res, next){
